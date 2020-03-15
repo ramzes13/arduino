2984e9a0067dc74c597b3f344bd0bcfc93c0829e
darii petru <darii.petru@gmail.com>
2020-03-15 14:08:08 +0200

    add signal generator

--- infrared-reader/infrared-reader.ino
+++ infrared-reader/infrared-reader.ino
@@ -26,8 +26,43 @@ void setup()
 
 void loop() {
   if (irrecv.decode(&results)) {
+    if (results.decode_type == NEC) {
+      Serial.print("NEC: ");
+    } else if (results.decode_type == SONY) {
+      Serial.print("SONY: ");
+    } else if (results.decode_type == RC5) {
+      Serial.print("RC5: ");
+    } else if (results.decode_type == RC6) {
+      Serial.print("RC6: ");
+    } else if (results.decode_type == UNKNOWN) {
+      Serial.print("UNKNOWN: ");
+    }
     Serial.println(results.value, HEX);
+    Serial.println(results;
+    Serial.println("Begin");
+    for(int i = 0; i < results.rawlen; i++)
+    {
+      Serial.print(results.rawbuf[i]);
+      Serial.print(", ");
+    }
+    Serial.println("\nEnd");
     irrecv.resume(); // Receive the next value
   }
   delay(100);
+
+
+
+  // irsend.sendNEC(IRcode, numBits);
+// Send a code in NEC format.
+
+// irsend.sendSony(IRcode, numBits);
+// Send a code in Sony format.
+
+// irsend.sendRC5(IRcode, numBits);
+// Send a code in RC5 format.
+
+// irsend.sendRC6(IRcode, numBits);
+// Send a code in RC6
+
+// irsend.sendRaw(rawbuf, rawlen, frequency);
 }
--- signal-generator/signal-generator.ino
+++ signal-generator/signal-generator.ino
@@ -1,40 +1,91 @@
-#include "Arduino.h"
-#include "HardwareSerial.h"
-/*
-  ReadAnalogVoltage
+// High-accuracy square wave generator
+// based on ESP8266
+// with runtime adjustable frequency, PWM width and offset
+// Output wave at pin 5 (configurable from 0 to 15, but not 16)
+// by James Swineson <github@public.swineson.me>, 2017-10
+// https://gist.github.com/Jamesits/92394675c0fe786467b26f90e95d3904
+// See https://blog.swineson.me/implementation-of-6mbps-high-speed-io-on-esp8266-arduino-ide/
+// for more information (article in Chinese)
+// Arduino UNO version: https://gist.github.com/Jamesits/8d164818946a65d0cafcd6203e3e5049
 
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+#include <Esp.h>
+#include <esp8266_peri.h>
+#include <eagle_soc.h>
+#define WDT_CTL 0x60000900
+#define WDT_CTL_ENABLE (BIT(0))
 
-  This example code is in the public domain.
+// Configurable from 0 to 15; 16 not available
+// Please refer to your breakout board datasheet for pin mapping
+// WARNING: some pins are used internally for connecting the ESP to ROM chip;
+// DO NOT USE THEM or your ESP will be bricked
+#define PINOUT 5
 
-  http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
-*/
-const uint8_t pin = 5;
-bool up = true;
-// the setup routine runs once when you press reset:
-void setup()
+double freq;   // Hz
+double offset; // percent (0.0 to 1.0)
+double width;  // percent (0.0 to 1.0)
+
+// unit: microsecond
+unsigned long cycle_time;
+unsigned long raising_edge;
+unsigned long falling_edge;
+unsigned long prev_micros;
+
+// compare 2 unsigned value
+// true if X > Y while for all possible (X, Y), X - Y < Z
+#define TIME_CMP(X, Y, Z) (((X) - (Y)) < (Z))
+
+inline void setHigh()
 {
-  pinMode(pin, OUTPUT);
-  pinMode(pin, HIGH);
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  GPOS = (1 << PINOUT);
 }
 
-// the loop routine runs over and over again forever:
+inline void setLow()
+{
+  GPOC = (1 << PINOUT);
+}
+
+void setup()
+{
+  // disable hardware watchdog
+  CLEAR_PERI_REG_MASK(WDT_CTL, WDT_CTL_ENABLE);
+  // disable software watchdog
+  ESP.wdtDisable();
+  // set IO pin mode
+  pinMode(PINOUT, OUTPUT);
+
+  // calculate arguments
+  freq = 125000;
+  width = 0.5;
+  offset = 0;
+
+  cycle_time = 1000000 / freq;
+  raising_edge = (unsigned long)(offset * cycle_time) % cycle_time;
+  falling_edge = (unsigned long)((offset + width) * cycle_time) % cycle_time;
+
+  prev_micros = micros();
+}
+
 void loop()
 {
-  // Serial.println("up" + up);
-  // delay(1);
-  if (up)
+  if (width + offset < 1)
   {
-    digitalWrite(pin, LOW);
-    up = false;
+    // raising edge should appear earlier
+    while (TIME_CMP(micros(), prev_micros + raising_edge, cycle_time))
+      ;
+    setHigh();
+    while (TIME_CMP(micros(), prev_micros + falling_edge, cycle_time))
+      ;
+    setLow();
   }
   else
   {
-    digitalWrite(pin, HIGH);
-    up = true;
+    // falling edge should appear earlier
+    while (TIME_CMP(micros(), prev_micros + falling_edge, cycle_time))
+      ;
+    setLow();
+    while (TIME_CMP(micros(), prev_micros + raising_edge, cycle_time))
+      ;
+    setHigh();
   }
+  prev_micros += cycle_time;
 }
